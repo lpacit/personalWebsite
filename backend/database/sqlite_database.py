d7b1912218237b169d1918783d14f12db75f703c
Lorenzo Pacitto <lorenzo.pacitto97@gmail.com>
2025-07-18 23:53:29 +0200

    added db instructions

--- backend/database/sqlite_database.py
+++ backend/database/sqlite_database.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 from decouple import config
+from backend.app.env import DATABASE_PATH
 
-from backend.env import DATABASE_PATH
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 sqlite_path = config("DATABASE_PATH", default=DATABASE_PATH)
 SQL_ALCHEMY_DATABASE_URI = f"sqlite:///{sqlite_path}"
@@ -11,3 +11,5 @@ engine = create_engine(
     SQL_ALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
 )
 Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
