367e0380863fc895c2d952ac415d76e0edc9a9b5
Lorenzo Pacitto <PACITTOL@ext.essilor.com>
2025-07-21 21:37:52 +0200

    update: added user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
 
 class User(BaseModel):
-    username: str
-    password: str
+    user_email: str
+    username: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDb(User):
+    hashed_password: str
+
+def get_user(user_email: str):
+    pass
