495a3ecbd468361e13d1357d285496a684a1dfbc
Lorenzo Pacitto <PACITTOL@ext.essilor.com>
2025-07-21 21:37:33 +0200

    update: added functions

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordBearer
 
 from backend.app.models.user import User
 
 router = APIRouter(
     tags=["Login"]
 )
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @router.post("/login")
 async def login(item: User):
